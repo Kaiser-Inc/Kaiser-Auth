f066c0ba64b1e4a12adf17f5a095edc56c9aff8d
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-17 23:01:01 -0300

    feat: add register user use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,31 +1,31 @@
-import type { Prisma, User } from "@prisma/client";
-import { randomUUID } from "node:crypto";
-import type { UsersRepository } from "../users-repository";
+import type { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import type { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
-    public items: User[] = [] 
+  public items: User[] = []
 
-    async create(data: Prisma.UserCreateInput) {
-        const user: User = {
-            id: randomUUID(),
-            email: data.email,
-            name: data.name,
-            pasword_hash: data.pasword_hash,
-            created_at: new Date()
-        }
-
-        await this.items.push(user)
-
-        return user
+  async create(data: Prisma.UserCreateInput) {
+    const user: User = {
+      id: randomUUID(),
+      email: data.email,
+      name: data.name,
+      password_hash: data.password_hash,
+      created_at: new Date(),
     }
 
-    async findByEmail(email: string) {
-        const user = this.items.find((item) => item.email === email)
-    
-        if (!user) {
-          return null
-        }
-    
-        return user
-      }
+    await this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
 }
