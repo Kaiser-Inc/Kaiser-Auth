93f1d1ed8688d8fdc20af4d4b101e28bf3181bab
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-19 13:25:50 -0300

    Feat: add regsiter controler

---
+++ src/http/controlers/register.ts
@@ -0,0 +1,33 @@
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists-error";
+import { makeRegisterUseCase } from "@/services/factories/make-register-use-case";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function Register(request: FastifyRequest, reply: FastifyReply) {
+    const registerUserBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8)
+    })
+
+    const { name, email, password } = registerUserBodySchema.parse(request.body)
+
+    try {
+        const registerUseCase = makeRegisterUseCase()
+
+        await registerUseCase.execute({
+            name,
+            email,
+            password
+        })
+    } catch(err) {
+        if (err instanceof UserAlreadyExistsError){
+            return reply.status(409).send({
+                message: err.message,
+            })
+        }
+        return reply.status(500).send()
+    }
+
+    return reply.status(201).send
+}
--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,3 +1,6 @@
 import type { FastifyInstance } from 'fastify'
+import { Register } from '../controlers/register'
 
-export async function appRoutes(app: FastifyInstance) {}
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/users', Register)
+}
