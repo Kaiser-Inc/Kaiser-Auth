f35c0757d0fdcacca35139bd064d675defb0f3c1
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-28 10:35:42 -0300

    feat(profile): add prisma repository find by id method and profile factory

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,16 @@
 import { prisma } from '@/lib/prisma'
-import type { Prisma } from '@prisma/client'
+import type { Prisma, User } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
+  }
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
